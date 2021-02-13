4b1db0d8907499472b90a9409d7ee15ca643e84c
UnderNickOfficial <funtimelnz@gmail.com>
2021-02-13 11:48:40 +0200

    Added ChangeBackgroundColor

--- WinFormsForRepository/WinFormsForRepository/Form1.cs
+++ WinFormsForRepository/WinFormsForRepository/Form1.cs
@@ -16,5 +16,10 @@ namespace WinFormsForRepository
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.Orange;
+        }
     }
 }
